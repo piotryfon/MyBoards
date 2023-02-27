7162c870c65084bb785533035393ca460bcb3753
piotryfon <pit.pit@wp.pl>
2023-02-27 11:42:07 +0100

    Dodaj MyBoardsDbContext i skonfiguruj połaączenie do DB.

---
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBoards.Entities
+{
+    public class MyBoardsContext : DbContext
+    {
+        public MyBoardsContext(DbContextOptions<MyBoardsContext>options) : base(options)
+        {
+
+        }
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;DataBase=MyBoardsDb;Trusted_Connection=True");
+        //}
+    }
+}
